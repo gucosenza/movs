eb801fc6ab7f957be503f96c1b4f303d3686c57d
Gustavo Cosenza <gucosenza@gmail.com>
2020-09-28 03:27:18 -0300

    favorites na view

---
+++ Movs/Components/Favorites/FavoritesView.swift
@@ -0,0 +1,81 @@
+
+import UIKit
+
+class FavoritesView: UIView {
+    
+    var favoritesViewDelegate: FavoritesViewDelegate!
+
+    lazy var tableView: UITableView = {
+        let table = UITableView()
+        table.translatesAutoresizingMaskIntoConstraints = false
+//        table.separatorColor = .white
+        table.separatorStyle = .none
+        return table
+    }()
+    
+    lazy var stackView: UIStackView = {
+        let stackView = UIStackView(frame: .zero)
+        stackView.axis = .vertical
+        stackView.spacing = 0
+        stackView.alignment = .fill
+        stackView.distribution = .fill
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        return stackView
+    }()
+    
+    private lazy var removeFilterButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setTitle("Remove Filter", for: .normal)
+        button.setTitleColor(UIColor(named: "colorYellow"), for: .normal)
+        button.titleLabel?.font = UIFont(name: "System", size: 20)
+        button.backgroundColor = UIColor(named: "colorDarkBlue")
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.addTarget(self, action: #selector(removeFilterClicked), for: .touchUpInside)
+        return button
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        removeFilterHidden(isHidden: true)
+        setupViews()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    @objc private func removeFilterClicked() {
+        removeFilterHidden(isHidden: true)
+        favoritesViewDelegate.removeFilter()
+    }
+    
+    func removeFilterHidden(isHidden: Bool) {
+        removeFilterButton.isHidden = isHidden
+    }
+    
+}
+
+extension FavoritesView: CodeView {
+    func buildViewHierarchy() {
+        self.addSubview(stackView)
+        stackView.addArrangedSubview(removeFilterButton)
+        stackView.addArrangedSubview(tableView)
+    }
+    
+    func setupConstraints() {
+        stackView.topAnchor.constraint(equalTo: self.safeAreaLayoutGuide.topAnchor, constant: 0).isActive = true
+        stackView.leadingAnchor.constraint(equalTo: self.safeAreaLayoutGuide.leadingAnchor, constant: 0).isActive = true
+        stackView.trailingAnchor.constraint(equalTo: self.safeAreaLayoutGuide.trailingAnchor, constant: 0).isActive = true
+        stackView.bottomAnchor.constraint(equalTo: self.safeAreaLayoutGuide.bottomAnchor, constant: 0).isActive = true
+        
+        removeFilterButton.heightAnchor.constraint(equalToConstant: 40).isActive = true
+        removeFilterButton.topAnchor.constraint(equalTo: stackView.topAnchor, constant: 0).isActive = true
+        removeFilterButton.leadingAnchor.constraint(equalTo: stackView.leadingAnchor, constant: 0).isActive = true
+        removeFilterButton.trailingAnchor.constraint(equalTo: stackView.trailingAnchor, constant: 0).isActive = true
+
+        tableView.topAnchor.constraint(equalTo: removeFilterButton.bottomAnchor, constant: 0).isActive = true
+        tableView.leadingAnchor.constraint(equalTo: stackView.leadingAnchor, constant: 0).isActive = true
+        tableView.trailingAnchor.constraint(equalTo: stackView.trailingAnchor, constant: 0).isActive = true
+        tableView.bottomAnchor.constraint(equalTo: stackView.bottomAnchor, constant: 0).isActive = true
+    }
+}
--- Movs/Components/Favorites/FavoritesViewController.swift
+++ Movs/Components/Favorites/FavoritesViewController.swift
@@ -6,116 +6,59 @@ protocol ApplyFilterActionDelegate {
     func applyFilter()
 }
 
+protocol FavoritesViewDelegate {
+    func removeFilter()
+}
+
 class FavoritesViewController: UIViewController {
     
     private let favoriteManager = FavoriteManager.shared
     private let favoriteTableViewManager = FavoriteTableViewManager()
     let filterViewController = FilterViewController()
     
-    lazy var tableView: UITableView = {
-            let table = UITableView()
-            table.translatesAutoresizingMaskIntoConstraints = false
-    //        table.separatorColor = .white
-            table.separatorStyle = .none
-            return table
-        }()
-    
-    private lazy var stackView: UIStackView = {
-        let stackView = UIStackView(frame: .zero)
-        stackView.axis = .vertical
-        stackView.spacing = 0
-        stackView.alignment = .fill
-        stackView.distribution = .fill
-        stackView.translatesAutoresizingMaskIntoConstraints = false
-        return stackView
-    }()
-    
-    private lazy var removeFilterButton: UIButton = {
-        let button = UIButton(type: .system)
-        button.setTitle("Remove Filter", for: .normal)
-        button.setTitleColor(UIColor(named: "colorYellow"), for: .normal)
-        button.titleLabel?.font = UIFont(name: "System", size: 20)
-        button.backgroundColor = UIColor(named: "colorDarkBlue")
-        button.translatesAutoresizingMaskIntoConstraints = false
-        button.addTarget(self, action: #selector(removeFilterClicked), for: .touchUpInside)
-        return button
-    }()
+    let favoritesView = FavoritesView()
 
     override func loadView() {
         super.loadView()
-               
+        self.view = favoritesView
+        
+        self.title = "Favorites"
         navigationController!.navigationBar.tintColor = .black
         navigationController!.navigationBar.isTranslucent = false
         navigationController!.navigationBar.barTintColor = UIColor(named: "colorYellow")
         let filterButton = UIBarButtonItem(image: UIImage(named: "FilterIcon"), style: .plain, target: self, action: #selector(filterClicked))
         filterButton.tintColor = .black
         self.navigationItem.rightBarButtonItem  = filterButton
-        
-        removeFilterHidden(isHidden: true)
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        tableView.dataSource = favoriteTableViewManager
-        tableView.delegate = favoriteTableViewManager
-        tableView.register(FavoriteTableViewCell.self, forCellReuseIdentifier: reuseIdentifier)
-        
-        setupViews()
+        favoritesView.tableView.dataSource = favoriteTableViewManager
+        favoritesView.tableView.delegate = favoriteTableViewManager
+        favoritesView.tableView.register(FavoriteTableViewCell.self, forCellReuseIdentifier: reuseIdentifier)
+
         filterViewController.applyFilterActionDelegate = self
+        favoritesView.favoritesViewDelegate = self
     }
     
     override func viewWillAppear(_ animated: Bool) {
         DispatchQueue.main.async {
             self.favoriteManager.getFavorites()
-            self.tableView.reloadData()
+            self.favoritesView.tableView.reloadData()
         }
     }
     
     @objc private func filterClicked() {
         navigationController!.pushViewController(filterViewController, animated: true)
     }
-    
-    @objc private func removeFilterClicked() {
-        favoriteTableViewManager.isFiltering = false
-        removeFilterHidden(isHidden: true)
-        self.tableView.reloadData()
-    }
-    
-    func removeFilterHidden(isHidden: Bool) {
-        removeFilterButton.isHidden = isHidden
-    }
 
 }
 
-extension FavoritesViewController: CodeView {
-    func buildViewHierarchy() {
-        self.view.addSubview(stackView)
-        stackView.addArrangedSubview(removeFilterButton)
-        stackView.addArrangedSubview(tableView)
-    }
-    
-    func setupConstraints() {
-        
-        stackView.topAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.topAnchor, constant: 0).isActive = true
-        stackView.leadingAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.leadingAnchor, constant: 0).isActive = true
-        stackView.trailingAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.trailingAnchor, constant: 0).isActive = true
-        stackView.bottomAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.bottomAnchor, constant: 0).isActive = true
-        
-        removeFilterButton.heightAnchor.constraint(equalToConstant: 40).isActive = true
-        removeFilterButton.topAnchor.constraint(equalTo: stackView.topAnchor, constant: 0).isActive = true
-        removeFilterButton.leadingAnchor.constraint(equalTo: stackView.leadingAnchor, constant: 0).isActive = true
-        removeFilterButton.trailingAnchor.constraint(equalTo: stackView.trailingAnchor, constant: 0).isActive = true
-
-        tableView.topAnchor.constraint(equalTo: removeFilterButton.bottomAnchor, constant: 0).isActive = true
-        tableView.leadingAnchor.constraint(equalTo: stackView.leadingAnchor, constant: 0).isActive = true
-        tableView.trailingAnchor.constraint(equalTo: stackView.trailingAnchor, constant: 0).isActive = true
-        tableView.bottomAnchor.constraint(equalTo: stackView.bottomAnchor, constant: 0).isActive = true
-        
-    }
-    
-    func setupAdditionalConfiguration() {
-        self.title = "Favorites"
+extension FavoritesViewController: FavoritesViewDelegate {
+    func removeFilter() {
+        favoriteTableViewManager.isFiltering = false
+        self.favoritesView.tableView.reloadData()
     }
 }
 
@@ -123,6 +66,6 @@ extension FavoritesViewController: ApplyFilterActionDelegate {
     func applyFilter() {
         self.favoriteManager.filterFavorite()
         self.favoriteTableViewManager.isFiltering = true
-        self.removeFilterButton.isHidden = false
+        self.favoritesView.removeFilterHidden(isHidden: false)
     }
 }
